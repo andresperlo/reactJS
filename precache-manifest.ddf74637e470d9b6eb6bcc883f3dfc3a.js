ce2c60538100d6274ff79e0a9eb2350beeb28ff4
andresperlo <andres_perlo@hotmail.com>
2020-03-26 16:34:32 -0300

    Updates

---
+++ precache-manifest.ddf74637e470d9b6eb6bcc883f3dfc3a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c4e581bceb71538a7d515ff28a9b33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a743f242e5f87ddb3872",
+    "url": "/static/css/main.1f80b5dc.chunk.css"
+  },
+  {
+    "revision": "39dd699386e3bf8aba7d",
+    "url": "/static/js/2.bd6e02ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bd6e02ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a743f242e5f87ddb3872",
+    "url": "/static/js/main.e9d7cb42.chunk.js"
+  },
+  {
+    "revision": "3755639dbeb867e8e401",
+    "url": "/static/js/runtime-main.ad422c2e.js"
+  },
+  {
+    "revision": "3945053263332accde0e39b2de8f5bc9",
+    "url": "/static/media/icomoon.39450532.woff"
+  },
+  {
+    "revision": "7083b8c447fec0ed84d30356bd7a9e50",
+    "url": "/static/media/icomoon.7083b8c4.svg"
+  },
+  {
+    "revision": "be13ca93410f9616a45043f75e32190a",
+    "url": "/static/media/icomoon.be13ca93.ttf"
+  },
+  {
+    "revision": "ff58dd0121d1684948fed3750638a05e",
+    "url": "/static/media/icomoon.ff58dd01.eot"
+  },
+  {
+    "revision": "5d7dbc62d8e3d7c3844eb3d6ecb3b868",
+    "url": "/static/media/imagenPrincipal.5d7dbc62.jpg"
+  },
+  {
+    "revision": "6b98dd27b246b88e5dd442a4aca3d24e",
+    "url": "/static/media/menu1.6b98dd27.jpg"
+  },
+  {
+    "revision": "d47d3aecd649bfa483f8bb0a857f3ad9",
+    "url": "/static/media/menu2.d47d3aec.jpg"
+  },
+  {
+    "revision": "0dadf31e1bfabdb0760c7f52dd902273",
+    "url": "/static/media/menu3.0dadf31e.jpg"
+  }
+]);
